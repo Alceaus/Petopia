5409d709f1632183edd8718877f3f474a19203f9
lexilure <alexandrapadua01@gmail.com>
2023-07-20 15:05:22 +0800

    js homepage

---
+++ Homepage/index.js
@@ -0,0 +1,12 @@
+const petButton = document.querySelector("#petButton");
+const createPostButton= document.querySelector("#createPost");
+const loginButton=document.querySelector("#login");
+
+petButton.addEventListener("click",function){
+    e.preventDefault();
+
+}
+function viewpet(){
+
+}
+
