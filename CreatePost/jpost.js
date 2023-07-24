09afe1c25934275a458b1fb2a7ddac9952434159
johnnyzuniga <john_isaac_zuniga@dlsu.edu.ph>
2023-07-24 12:05:24 +0800

    Update jpost.js

--- CreatePost/jpost.js
+++ CreatePost/jpost.js
@@ -8,58 +8,52 @@ const error = document.getElementById('error')
 
 
 form.addEventListener('submit',(e)=> {
-    var errors = 0;
+    var errors = false;
     //name
     if(uname.value == null || uname.value === ''){
         uname.classList.add('error');
-        errors++;
+        errors = true;
     }else{
         uname.classList.remove('error');
-        errors--;
     }
 
     //fur
     if(fur.value == null || fur.value === ''){
         fur.classList.add('error');
-        errors++;
+        errors = true;
     }else{
         fur.classList.remove('error');
-        errors--;
     }
 
     //weight
     if(weight.value == null || weight.value === ''){
         weight.classList.add('error');
-        errors++;
+        errors = true;
     }else{
         weight.classList.remove('error');
-        errors--;
     }
 
     //likes
     if(likes.value == null || likes.value === ''){
         likes.classList.add('error');
-        errors++;
+        errors = true;
     }else{
         likes.classList.remove('error');
-        errors--;
     }
 
     //dislikes
     if(dlikes.value == null || dlikes.value === ''){
         dlikes.classList.add('error');
-        errors++;
+        errors = true;
     }else{
         dlikes.classList.remove('error');
-        errors--;
     }
 
     if (errors!=0){
         e.preventDefault();
-        error.innerHTML = "Fill out missing fields";
+        error.innerHTML = "Fill out the missing fields";
     }
     else{
-        errors = 0;
         error.innerHTML=" ";
     }
     
