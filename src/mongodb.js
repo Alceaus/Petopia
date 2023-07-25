2bfbd3a416a10defe0d8fd6ad33f619322b0512f
lexilure <alexandrapadua01@gmail.com>
2023-07-25 19:41:21 +0800

    index update for login

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/Petopia")
+mongoose.connect("mongodb://localhost:27017/Petopia") //mongo db connection to mongodb
 .then(()=>{
     console.log("mongodb connected");
 })
